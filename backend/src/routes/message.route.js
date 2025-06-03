8afb60768ee8168e7164ad5753469032b330dcb1
dhruv <dhruvdalwadi05@gmail.com>
2025-06-03 19:16:04 +0530

    final

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { getMessages, getUserForSidebar, sendMessage } from '../controllers/message.controller.js';
+import { getMessages, getUserForSidebar , sendMessage } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
-router.get("/users", protectRoute, getUserForSidebar);
-router.get("/messages/:id", protectRoute, getMessages);  // add static prefix "messages"
-router.post("/messages/send/:id", protectRoute, sendMessage);  // add static prefix "messages/send"
+
+router.get ("/users", protectRoute, getUserForSidebar);
+router.get ("/:id", protectRoute, getMessages); // Assuming you want to get a specific user by ID
+
+router.post ("/send/:id", protectRoute, sendMessage); // Assuming you want to send a message to a specific user by ID
 
 export default router;
